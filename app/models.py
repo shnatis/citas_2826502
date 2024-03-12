266b2dc82f34e8af687816fca6cbde6b5bc5bab2
Aprendiz <Aprendiz@BOGDFPCGMF4137.FormacionSena.RED>
2024-03-12 17:10:35 -0500

    trabajo

--- app/models.py
+++ app/models.py
@@ -15,7 +15,7 @@ class Medico(db.Model):
     registro_medico = db.Column(db.Integer)
     especialidad = db.Column(db.String(50))
 
-    citas = db.relationship("Cita" , backref = "medico" )
+    citas = db.relationship("Cita" , backref = "medicos" )
 
 
 class Paciente(db.Model):
--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,6 @@
-from . import app 
-from .models import Medico
-from .models import Paciente
-from .models import Consultorio
-from .models import Cita
-from flask import render_template
+from . import app, db
+from .models import *
+from flask import render_template, request
 
 
 #crear ruta para ver los medicos 
@@ -24,3 +21,48 @@ def get_all_consultorios():
     consultorios = Consultorio.query.all()
     return render_template("pacientes.html" , consultorios=consultorios )
 
+#crear ruta traer el medico por id (get)
+@app.route("/medicos/<int:id>")
+def get_medico_by_id(id):
+    medico = Medico.query.get(id)
+    return render_template("medico.html",
+                           med = medico)
+
+@app.route("/pacientes/<int:id>")
+def get_paciente_by_id(id):
+    paciente = Paciente.query.get(id)
+    return render_template("paciente.html",
+                           pac = paciente)
+
+#crear ruta para crear nuevo medico
+@app.route("/medicos/create" , methods = [ 'GET' , 'POST'] )
+def create_medico():
+    #######
+    #### mostrar el formulario: metodo GET 
+    #######
+    if( request.method == 'GET' ):
+        especialidades = [
+            "Cardiologia",
+            "Pediatria", 
+            "Oncologia"
+        ]
+        return render_template("medico_form.html",
+                                especialidades = especialidades )
+    
+    ####
+    ## Cuando el usuario presiona el boton guardar 
+    ## los datos del formulario viajan al servidor
+    ## utilizando el metodo POST
+
+    elif(request.method == 'POST'):
+        #cuando se presiona 'guardar'
+        new_medico = Medico(nombre = request.form["nombre"], 
+                            apellidos = request.form["apellidos"],
+                            tipo_identificacion = request.form["ti"],
+                            numero_identificacion = request.form["ni"],
+                            registro_medico = request.form["rm"],
+                            especialidad = request.form["es"]
+                            )
+        db.session.add(new_medico)
+        db.session.commit()
+        return "medico registrado" 
